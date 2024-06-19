22be5814f425a568ae166a97442b308db4a4bfc9
Rene Ruano <rene.ruano95@gmail.com>
2024-06-19 15:43:27 -0400

    chore: modify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
+  webpack(config) {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "liveblocks.io", port: "" },
